0c6e425f24e0742b39aff0c4ee659482fa77cb5a
rishav <rishav.21m@gmail.com>
2020-04-27 13:26:55 +0530

    changed to load model from server

--- initialize.py
+++ initialize.py
@@ -4,8 +4,10 @@ from helper import load_obj
 
 
 
-EMBEDDING_FILE = 'GoogleNews-vectors-negative300.bin.gz'
+print("loading model")
+EMBEDDING_FILE = 'https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz'
 modelg= KeyedVectors.load_word2vec_format(EMBEDDING_FILE, binary=True,limit=40000)
+print("model loaded")
 
 url='https://raw.githubusercontent.com/Pratikmehta1729/walkover/master/eng_sites_dataset_2.0_shuffled.csv'
 
