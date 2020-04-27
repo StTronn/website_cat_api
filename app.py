677fea307060abfb41967fa647754793b4cdd988
rishav <rishav.21m@gmail.com>
2020-04-27 14:19:08 +0530

    added logging

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from initialize import kmeans,X,labels,centroids,modelg,titles,l
 from flask_cors import CORS, cross_origin
 
+print("in app")
+
 app = Flask(__name__)
 from get_result import finalFunction
 cors = CORS(app)
