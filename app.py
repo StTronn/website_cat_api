0f947e1fa68bbcdde87f409125a2b13110c23574
rishav <rishav.21m@gmail.com>
2020-04-27 13:56:53 +0530

    changed to full mode

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-#from initialize import kmeans,X,labels,centroids,modelg,titles,l
+from initialize import kmeans,X,labels,centroids,modelg,titles,l
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-#from get_result import finalFunction
+from get_result import finalFunction
 cors = CORS(app)
 app.config['CORS_HEADERS'] = '*'
 
