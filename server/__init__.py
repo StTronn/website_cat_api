ed687718f8899c2e720a14cb89bef9ebb8af8341
rishav <rishav.21m@gmail.com>
2020-05-05 15:36:27 +0530

    added cluster websites route

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,13 @@
+import pandas as pd
 from flask import Flask
 from server.initialize import kmeans,X,labels,centroids,modelg,titles,l
 from flask_cors import CORS, cross_origin
+import json
 
 print("in app")
+
+csv_url="websites_data_csv.zip"
+sites_df=pd.read_csv(csv_url)
 
 app = Flask(__name__)
 from server.get_result import finalFunction
@@ -15,4 +20,16 @@ app.config['CORS_HEADERS'] = '*'
 def result_url(url=''):
     return finalFunction(url)
 
+@app.route('/getclusterurl/<int:centroid_no>/page/<int:page_no>')
+@app.route('/getclusterurl/<int:centroid_no>/')
+@app.route('/getclusterurl/')
+@cross_origin()
+def get_cluster_url(centroid_no=-1,page_no=1):
+    limit = len(sites_df)
+    start = (page_no-1)*10
+    end=page_no*10
+    if centroid_no<0 or centroid_no >100:
+        return json.dumps(sites_df[start:end][['rank','url']].to_dict('records'))
+    cluster_df=sites_df[sites_df.centroid_no==centroid_no]
+    return json.dumps(cluster_df[start:end][['rank','url']].to_dict('records'))
 
---
+++ server/search.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import pickle
+import numpy as np
+from gensim.models import Word2Vec,KeyedVectors
+from sklearn import cluster,metrics
+import gensim.downloader as api
+from urllib.parse import urlparse
+import scipy
+def load_obj(filename):
+  with open(filename,'rb') as obj_file:
+    obj = pickle.load(obj_file)
+    return obj
+
+
+
+def read_csv(csv_url):
+    df = pd.read_csv(csv_url)
+    sentences=[]
+    x=df.content.tolist()
+    y=df.url.tolist()
+    z=df['rank'].tolist()
+    c=df.centroid_no.tolist()
+    for item in x:
+        sentences.append(item.split())
+    return sentences,y,z,c
+
+
+def sent_vectorizer(sent, modelg):
+    sent_vec =[]
+    numw = 0
+    for w in sent:
+        try:
+            if numw == 0:
+                sent_vec = modelg[w]
+            else:
+                sent_vec = np.add(sent_vec, modelg[w])
+            numw+=1
+        except:
+            pass
+    return np.asarray(sent_vec) / numw
+
+def create_vector(sentences):
+  X=[]
+  for sentence in sentences:
+     X.append(sent_vectorizer(sentence, modelg))
+  return X
+
+def get_centroid_websites(num=-1):
+  if num==-1:
+    return X,y,z,centroid_no
+  else:
+    new_X=[]
+    new_Y=[]
+    new_Z=[]
+    new_C=[]
+    for i in range (len(y)):
+      if(centroid_no[i]==num):
+        new_X.append(X[i])
+        new_Y.append(y[i])
+        new_Z.append(z[i])
+        new_C.append(c[i])
+    return new_X,new_Y,new_Z,new_C
+
+def search_by_query(query,centroid_no=-1,no_of_results=20):
+    vectors,urls,ranks,centroid_num=get_centroid_websites(centroid_no)
+    query_vector=sent_vectorizer(query,modelg)
+    similarity=[]
+    try:
+     similarity=[scipy.spatial.distance.cosine(query_vector,x) for x in vectors]
+    except:
+      pass
+    top_idx = np.argsort(similarity)[0:no_of_results]
+    top_url= [[urls[i],ranks[i],similarity[i]] for i in top_idx]
+    rank_list=[(0.5*t[1])+(0.5*t[2]) for t in top_url]
+    top_rank_idx = np.argsort(rank_list)
+    final_rank=[top_url[i][0] for i in top_rank_idx]
+    return final_rank
+
+
+
+def search_by_domain(query_domain,centroid_no=-1):
+      X,y,z,c=get_centroid_websites(centroid_no)
+
+      domains=[]
+      for url in y:
+        d=urlparse(url).netloc
+        for word in query_domain:
+          if word in d:
+            domains.append(url)
+      return domains
+
+
+
+
+modelg = load_obj('modelg')
+
+csv_url="https://github.com/StTronn/website_cat_api/raw/master/websites_data_csv.zip"
+sentences,y,z,centroid_no=read_csv(csv_url)
+X=create_vector(sentences)
+
+
+print(search_by_query(["search"]))
